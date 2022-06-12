5f6eba8d2616b4dba3c4b239d8a6d59fa82db9f7
skvoren <skvoren@outlook.com>
2022-06-12 19:44:03 +0400

    exercise 1.9

--- unicafe/src/Statistics.jsx
+++ unicafe/src/Statistics.jsx
@@ -2,6 +2,8 @@ import React from "react";
 
 const Statistics = ({statData}) => {
 
+    const title = 'statistics'
+
     const [good, neutral, bad, clicks] = statData;
 
     const averageScore = (good, bad, clicks) => {
@@ -23,19 +25,26 @@ const Statistics = ({statData}) => {
         return result = (good / clicks) * 100
       }
 
-    return (
-        <div>
-            <h2>statistics</h2>
-            <p>good {good}</p>
-            <p>neutral {neutral}</p>
-            <p>bad {bad}</p>
-            <p>all {clicks}</p>
-            <p>average {averageScore(good, bad, clicks)}</p>
-            <p>positive {positiveFeedbackPercent(good, clicks)} %</p>
-        </div>
-
-    )
-    
+    if (clicks > 0) {
+        return (
+            <div>
+                <h2>{title}</h2>
+                <p>good {good}</p>
+                <p>neutral {neutral}</p>
+                <p>bad {bad}</p>
+                <p>all {clicks}</p>
+                <p>average {averageScore(good, bad, clicks)}</p>
+                <p>positive {positiveFeedbackPercent(good, clicks)} %</p>
+            </div>
+        ) 
+    } else {
+        return (
+            <div>
+                <h2>{title}</h2>
+                <p>No feedback given</p>
+            </div>
+        )
+    }
 }
 
 export default Statistics
